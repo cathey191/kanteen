54aaf5425a5d9aa54d69ed7c0dac58f32a127c46
cathey191 <cathey191@hotmail.com>
2018-03-22 11:52:14 +1300

    Google map!!

--- js/script.js
+++ js/script.js
@@ -1,5 +1,25 @@
+google.maps.event.addDomListener(window, 'load', init);
+function init(){
+	var mapOptions = {
+		center:{
+			lat: -41.280975,
+			lng:174.779384
+		},
+		//states what the initial zoom for the map is.
+		zoom: 17
+	}
+	new google.maps.Map(document.getElementById("map"), mapOptions);
+}
+
 $(document).ready(function(){
 	$(".burgerMenu").click(function(){
 		$("#sideNav").toggle();
 	});
+
+	$(".tab").click(function(){
+		$(".tabOn").addClass("tabs");
+		$(".tabOn").removeClass("tabOn");
+		$(this).toggleClass("tabs");
+		$(this).toggleClass("tabOn");
+	})
 });
